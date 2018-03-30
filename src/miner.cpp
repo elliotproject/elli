6cb8c48b148aa9d3a3f79e4923c5c049817abae3
elliotproject <elliotproject@outlook.com>
2018-03-30 21:08:14 +0000

    [Mining][Readme]
    - Compute final coinbase transaction fix
    - Add travis.ci status to readme

--- src/miner.cpp
+++ src/miner.cpp
@@ -431,7 +431,13 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
         LogPrintf("CreateNewBlock(): total size %u\n", nBlockSize);
 
         // Compute final coinbase transaction.
-        pblock->vtx[0].vin[0].scriptSig = CScript() << nHeight << OP_0;
+		if (nHeight > Params().LAST_POW_BLOCK())
+		{
+			pblock->vtx[0].vin[0].scriptSig = CScript() << nHeight << OP_0;
+		} else {
+			txNew.vin[0].scriptSig = CScript() << nHeight << OP_0;
+		}
+        
         if (!fProofOfStake) {
             pblock->vtx[0] = txNew;
             pblocktemplate->vTxFees[0] = -nFees;
