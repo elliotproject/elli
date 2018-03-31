16cb9aa6d1d5ea0fe6668d08d0aa0c6f8360146b
elliotproject <elliotproject@outlook.com>
2018-03-31 12:36:59 +0000

    [Qt] fix negative balance

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -209,8 +209,8 @@ void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmed
         nWatchOnlyLockedBalance = pwalletMain->GetLockedWatchOnlyBalance();
     }
     // ELLI Balance
-    CAmount nTotalBalance = balance + unconfirmedBalance;
-    CAmount elliAvailableBalance = balance - immatureBalance - nLockedBalance;
+    CAmount nTotalBalance = balance + unconfirmedBalance + immatureBalance + nLockedBalance;
+    CAmount elliAvailableBalance = balance;
     CAmount nTotalWatchBalance = watchOnlyBalance + watchUnconfBalance + watchImmatureBalance;    
     CAmount nUnlockedBalance = nTotalBalance - nLockedBalance;
     // zELLI Balance
