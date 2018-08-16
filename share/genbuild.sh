db4d26c87a82c00f635f08e2a0c6f49a3e0ea346
elliotproject <elliotproject@outlook.com>
2018-08-16 15:32:02 +0300

    fix genbuild for accordance to elli code convention

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
-if [ $# -gt 1 ]; then
+#!/usr/bin/env bash
+
+if [ "$#" -gt 1 ]; then
     cd "$2"
 fi
-if [ $# -gt 0 ]; then
+if [ "$#" -gt 0 ]; then
     FILE="$1"
     shift
-    if [ -f "$FILE" ]; then
-        INFO="$(head -n 1 "$FILE")"
+    if [ ! -z "$FILE" ] && [ -f "$FILE" ]; then
+        INFO="$(head --lines=1 $FILE)"
     fi
 else
     echo "Usage: $0 <filename> <srcroot>"
@@ -18,25 +19,25 @@ SUFFIX=""
 LAST_COMMIT_DATE=""
 if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
     # clean 'dirty' status of touched files that haven't been modified
-    git diff >/dev/null 2>/dev/null 
-
+    git diff >/dev/null 2>&1
+    
     # if latest commit is tagged and not dirty, then override using the tag name
     RAWDESC=$(git describe --abbrev=0 2>/dev/null)
-    if [ "$(git rev-parse HEAD)" = "$(git rev-list -1 $RAWDESC)" ]; then
-        git diff-index --quiet HEAD -- && DESC=$RAWDESC
+    if [ ! -z "$RAWDESC" ] && [ "$(git rev-parse HEAD)" = "$(git rev-list -1 $RAWDESC)" ]; then
+        git diff-index --quiet HEAD -- && DESC="$RAWDESC"
     fi
-
+    
     # otherwise generate suffix from git, i.e. string like "59887e8-dirty"
     SUFFIX=$(git rev-parse --short HEAD)
     git diff-index --quiet HEAD -- || SUFFIX="$SUFFIX-dirty"
-
+    
     # get a string like "2012-04-10 16:27:19 +0200"
     LAST_COMMIT_DATE="$(git log -n 1 --format="%ci")"
 fi
 
 if [ -n "$DESC" ]; then
     NEWINFO="#define BUILD_DESC \"$DESC\""
-elif [ -n "$SUFFIX" ]; then
+    elif [ -n "$SUFFIX" ]; then
     NEWINFO="#define BUILD_SUFFIX $SUFFIX"
 else
     NEWINFO="// No build information available"
@@ -44,7 +45,7 @@ fi
 
 # only update build.h if necessary
 if [ "$INFO" != "$NEWINFO" ]; then
-    echo "$NEWINFO" >"$FILE"
+    echo "$NEWINFO" > "$FILE"
     if [ -n "$LAST_COMMIT_DATE" ]; then
         echo "#define BUILD_DATE \"$LAST_COMMIT_DATE\"" >> "$FILE"
     fi
