3373b5d99e4560e2c14a84f91ba1312952a02e2e
elliotproject <elliotproject@outlook.com>
2018-08-29 00:35:43 +0300

    update build script

--- contrib/build-scripts/build-x86_64-linux-gnu.sh
+++ contrib/build-scripts/build-x86_64-linux-gnu.sh
@@ -2,14 +2,15 @@
 
 cd ../..
 CORES=$(nproc --all)
-HOST="x86_64-linux-gnu"
+HOST="x86_64-pc-linux-gnu"
 CONFIGFLAGS="--enable-reduce-exports --disable-bench --disable-tests --disable-gui-tests --disable-maintainer-mode --disable-dependency-tracking --enable-upnp-default --disable-zmq"
-HOST_CFLAGS="-march=native -funroll-loops"
-HOST_CXXFLAGS="-march=native -funroll-loops"
+HOST_CFLAGS="-Os"
+HOST_CXXFLAGS="-Os"
 HOST_LDFLAGS="-static-libstdc++"
 BASEPREFIX="$PWD/depends"
+DEP_OPTS="NO_QT=0 NO_UPNP=0 DEBUG=0 NO_WALLET=0"
 
-make HOST=${HOST} -C ${BASEPREFIX}
+make HOST=${HOST} -C ${BASEPREFIX} ${DEP_OPTS}
 ./autogen.sh
 CONFIG_SITE=${BASEPREFIX}/${HOST}/share/config.site ./configure --prefix=/ ${CONFIGFLAGS} CFLAGS="${HOST_CFLAGS}" CXXFLAGS="${HOST_CXXFLAGS}" LDFLAGS="${HOST_LDFLAGS}"
 make --jobs ${CORES}
