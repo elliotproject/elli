f78a855764fb9e977ac5a9e6d61edce40550dd56
elliotproject <elliotproject@outlook.com>
2018-08-30 18:56:58 +0300

    fix script

--- contrib/build-scripts/build-x86_64-linux-gnu.sh
+++ contrib/build-scripts/build-x86_64-linux-gnu.sh
@@ -3,14 +3,13 @@
 cd ../..
 CORES=$(nproc --all)
 HOST="x86_64-pc-linux-gnu"
-CONFIGFLAGS="--enable-reduce-exports --disable-bench --disable-tests --disable-gui-tests --disable-maintainer-mode --disable-dependency-tracking --enable-upnp-default --disable-zmq"
-HOST_CFLAGS="-Os"
-HOST_CXXFLAGS="-Os"
+CONFIGFLAGS="--enable-reduce-exports --disable-bench --disable-tests --disable-gui-tests --disable-maintainer-mode --enable-upnp-default --disable-zmq"
+HOST_CFLAGS=""
+HOST_CXXFLAGS=""
 HOST_LDFLAGS="-static-libstdc++"
 BASEPREFIX="$PWD/depends"
-DEP_OPTS="NO_QT=0 NO_UPNP=0 DEBUG=0 NO_WALLET=0"
 
-make HOST=${HOST} -C ${BASEPREFIX} ${DEP_OPTS}
+make HOST=${HOST} -C ${BASEPREFIX}
 ./autogen.sh
 CONFIG_SITE=${BASEPREFIX}/${HOST}/share/config.site ./configure --prefix=/ ${CONFIGFLAGS} CFLAGS="${HOST_CFLAGS}" CXXFLAGS="${HOST_CXXFLAGS}" LDFLAGS="${HOST_LDFLAGS}"
 make --jobs ${CORES}
