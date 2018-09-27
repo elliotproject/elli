bf1b161ec612d0378245313b77902adf482b01a2
elliotproject <elliotproject@outlook.com>
2018-09-28 01:32:07 +0300

    update build script

--- contrib/build-scripts/build-i686-w64-mingw32.sh
+++ contrib/build-scripts/build-i686-w64-mingw32.sh
@@ -3,9 +3,9 @@
 cd ../..
 CORES=$(nproc --all)
 HOST="i686-w64-mingw32"
-CONFIGFLAGS="--enable-reduce-exports --disable-bench --disable-tests --disable-gui-tests --disable-maintainer-mode --disable-dependency-tracking --enable-upnp-default --disable-zmq"
-HOST_CFLAGS="-g"
-HOST_CXXFLAGS="-g"
+CONFIGFLAGS="--enable-reduce-exports --disable-bench --disable-tests --disable-gui-tests --disable-maintainer-mode --enable-upnp-default --disable-zmq"
+HOST_CFLAGS=""
+HOST_CXXFLAGS=""
 BASEPREFIX="$PWD/depends"
 
 make HOST=${HOST} -C ${BASEPREFIX}
