0ba3e94983e48134dfd0ae990eb89b6f1dead9a6
elliotproject <elliotproject@outlook.com>
2019-01-20 10:34:25 +0300

    new protocol version

--- src/main.cpp
+++ src/main.cpp
@@ -6694,9 +6694,6 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
-    if (IsSporkActive(SPORK_17_NEW_PROTOCOL_ENFORCEMENT_3)) {
-        return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2;
-    }
     return PROTOCOL_VERSION;
 }
 
